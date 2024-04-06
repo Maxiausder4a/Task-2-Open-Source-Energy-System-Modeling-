b2b1e7598607a44e76184c81e047bc2dbfd16465
Maxiausder4a <maxi.frey.uni@gmail.com>
2024-04-06 14:33:26 +0200

    Building the basic Code for a Photovoltaics self suffiency  calculation
    This change builds the first basic code for a calculation of the degree of self suffiency of a electricity consumer and a pv plant. The Code calculates the self sufficiency by simply comparing the yield of a pv plant and the consumption and choosing the smaller value which is then set as ownconsumption. The total amount of ownconsumption is divided by the total amount of power consumption to receive the total degree of self sufficiency.

--- Code.py
+++ Code.py
@@ -1,2 +1,39 @@
-#PV Ertrag 12 Werte fÃ¼r 12 h pro Tag: 2 0 3 7 3 8 4 6 5 0 1 2 
-E = 
+import numpy as np
+import matplotlib.pyplot as plt
+
+#PV Ertrag 12 Werte in kWh fuer 12 h pro Tag: 2 0 3 7 3 8 4 6 5 0 1 2 als 1 x 12 Matrix
+Ertrag = np.array([2,0,3,7, 3, 8, 4, 6, 5,0,1,2])
+Ertrag_ges = np.sum(Ertrag)
+
+#Strom Verbrauch des Verbrauchers in kWh, 12 Werte fuer 12 Stunden 
+# Erstellen von 3 Segmenten mit zufälligen Werten in den angegebenen Bereichen, mit morgens niedrigem, mittags und Abends etwas höherem Verbrauch 
+segment1 = np.random.randint(0, 7, size=3)  # 3 Werte zwischen 0 und 6
+segment2 = np.random.randint(2, 16, size=6) # 6 Werte zwischen 2 und 15
+segment3 = np.random.randint(0, 11, size=3) # 3 Werte zwischen 4 und 15
+
+# Kombinieren der zufälligen Segmente zu einer einzigen einzeiligen Matrix
+Stromverbrauch = np.concatenate([segment1, segment2, segment3])
+Stromverbrauch_ges = np.sum(Stromverbrauch)
+
+#Eigenverbrauch berechnen
+Eigenverbrauch = np.minimum (Ertrag,Stromverbrauch)
+Eigenverbrauch_ges = np.sum(Eigenverbrauch)
+
+#Autarkiegrad berechnen
+Autarkie = Eigenverbrauch_ges / Stromverbrauch_ges
+
+# Erstellen eines Arrays mit den Positionen der Werte für die X-Achse
+x_positionen = np.arange(len(Ertrag))
+
+# Plotten der Matrizen
+plt.plot(x_positionen, Ertrag, label='Ertrag', marker='o')
+plt.plot(x_positionen, Stromverbrauch, label='Stromverbrauch', marker='x')
+plt.plot(x_positionen, Eigenverbrauch, label='Eigenverbrauch', marker='^')
+
+
+# Hinzufügen von Titel und Legende
+plt.legend()
+
+# Anzeigen des Plots und des Autrakiegrads
+plt.show()
+print(Autarkie)
